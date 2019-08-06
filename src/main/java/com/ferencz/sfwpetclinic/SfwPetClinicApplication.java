479b4711f6f5e26fd31b8bdb8994fbace3262b82
drw-fan <ktf@mail.com>
2019-08-06 19:08:13 -0400

    Initial Project Add

---
+++ src/main/java/com/ferencz/sfwpetclinic/SfwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ferencz.sfwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfwPetClinicApplication.class, args);
+    }
+
+}
