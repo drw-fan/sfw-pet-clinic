8853457ac135f78534187ca6a58e2180a3699bd4
Katie Ferencz <ktf@mail.com>
2019-08-18 18:37:44 -0400

    added Pets List and updated controller. closes #15.

--- pet-clinic-web/src/main/java/com/ferencz/sfwpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ferencz/sfwpetclinic/controllers/OwnerController.java
@@ -16,9 +16,7 @@ public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model){
-
         model.addAttribute("owners", ownerService.findAll());
-
         return "owners/index";
     }
 }
